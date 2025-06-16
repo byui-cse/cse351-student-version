c80a759cdd3af2a5b2bec8e6a5e7b42a2e79b1b5
Luc Comeau <luc.comeau@gmail.com>
2025-06-16 05:07:20 +0000

    Assignment 10

---
+++ lesson_10/prove/functions.py
@@ -0,0 +1,85 @@
+"""
+Course: CSE 351, week 10
+File: functions.py
+Author: <your name>
+
+Instructions:
+
+Depth First Search
+https://www.youtube.com/watch?v=9RHO6jU--GU
+
+Breadth First Search
+https://www.youtube.com/watch?v=86g8jAQug04
+
+
+Requesting a family from the server:
+family_id = 6128784944
+data = get_data_from_server('{TOP_API_URL}/family/{family_id}')
+
+Example JSON returned from the server
+{
+    'id': 6128784944, 
+    'husband_id': 2367673859,        # use with the Person API
+    'wife_id': 2373686152,           # use with the Person API
+    'children': [2380738417, 2185423094, 2192483455]    # use with the Person API
+}
+
+Requesting an individual from the server:
+person_id = 2373686152
+data = get_data_from_server('{TOP_API_URL}/person/{person_id}')
+
+Example JSON returned from the server
+{
+    'id': 2373686152, 
+    'name': 'Stella', 
+    'birth': '9-3-1846', 
+    'parent_id': 5428641880,   # use with the Family API
+    'family_id': 6128784944    # use with the Family API
+}
+
+
+--------------------------------------------------------------------------------------
+You will lose 10% if you don't detail your part 1 and part 2 code below
+
+Describe how to speed up part 1
+
+<Add your comments here>
+
+
+Describe how to speed up part 2
+
+<Add your comments here>
+
+
+Extra (Optional) 10% Bonus to speed up part 3
+
+<Add your comments here>
+
+"""
+from common import *
+import queue
+
+# -----------------------------------------------------------------------------
+def depth_fs_pedigree(family_id, tree):
+    # KEEP this function even if you don't implement it
+    # TODO - implement Depth first retrieval
+    # TODO - Printing out people and families that are retrieved from the server will help debugging
+
+    pass
+
+# -----------------------------------------------------------------------------
+def breadth_fs_pedigree(family_id, tree):
+    # KEEP this function even if you don't implement it
+    # TODO - implement breadth first retrieval
+    # TODO - Printing out people and families that are retrieved from the server will help debugging
+
+    pass
+
+# -----------------------------------------------------------------------------
+def breadth_fs_pedigree_limit5(family_id, tree):
+    # KEEP this function even if you don't implement it
+    # TODO - implement breadth first retrieval
+    #      - Limit number of concurrent connections to the FS server to 5
+    # TODO - Printing out people and families that are retrieved from the server will help debugging
+
+    pass
